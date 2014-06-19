c031296339c28494c6978a042b901e43ae751638
Daniel Price <dannycansignup@gmail.com>
2014-06-19 14:17:47 -0400

    Free

--- models/users.js
+++ models/users.js
@@ -16,7 +16,6 @@ User.add({
 	email: { type: Types.Email, initial: true, required: true, index: true },
 	password: { type: Types.Password, initial: true, required: false },
 	image : { type : Types.LocalFile, dest : 'images'},
-
 }, 'Permissions', {
 	isAdmin: { type: Boolean, label: 'Can access Keystone' },
 	freeAccess : {type : Boolean, label : 'Free Analysis'}
