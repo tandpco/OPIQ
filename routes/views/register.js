4802beb0232ece8869f16b0392d9192015c3b8d4
Nate <natethepcspecialist@gmail.com>
2014-06-19 18:41:08 -0400

    update

--- routes/views/register.js
+++ routes/views/register.js
@@ -140,7 +140,7 @@ function build_user (fields, view, req, locals, res) {
 		User.model.findOne({email : fields.email}).exec(function(er, user){
 			var backlog = keystone.get(ip + 'backlog'), ans;
 
-			console.log('this is the ip in register' , req.connection.socket.remoteAddress);
+			console.log('this is the ip in register' , req.socket.remoteAddress);
 			
 			if(backlog){
 
