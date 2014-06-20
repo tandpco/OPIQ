068e562db456751bd5e796b3457f0e752fc29dce
Nate <natethepcspecialist@gmail.com>
2014-06-20 13:34:40 -0400

    fixed ajax call to add test

--- routes/updatedb/savequestion.js
+++ routes/updatedb/savequestion.js
@@ -3,7 +3,7 @@ var keystone = require('keystone');
 exports = module.exports = function(req, res) {
 	var Answer = keystone.list('Answer'),
 		post = req.body;
-	console.log('this is post in savequestion' , post);
+		
 	Answer.model.findOne({page : post.page, user : req.user._id, analysis : post.analysis}).exec(function (err, answer) {
 		if(err)console.log(err);
 
