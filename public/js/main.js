22f377749dcc9ff5b0bd8cda83eb6c99e12e3a1e
Daniel Price <dannycansignup@gmail.com>
2014-07-02 16:34:23 -0400

    Perfect Complete variable error

--- public/js/main.js
+++ public/js/main.js
@@ -364,7 +364,6 @@ function updateAllScores(){
 
 
 	$('.percent-complete').set(Math.round($('#outline div li.page-complete').length / Pages.length * 1000) / 10);
-		var $percentCompleteVal = $percentComplete.val();
 	$percentComplete.val(Math.round($('#outline div li.page-complete').length / Pages.length * 1000) / 10);
 	if ($percentComplete.val() != '100') {
 		$('#report > .header > h1').html('Your analysis is ' + $percentCompleteVal + '% complete.');
