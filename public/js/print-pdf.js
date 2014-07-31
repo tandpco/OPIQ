dfa5e69ec37b0a3771567bafaa26fa10a12a7471
Nathan <natethepcspecialist@gmail.com>
2014-07-31 14:34:16 -0400

    update pdf

--- public/js/print-pdf.js
+++ public/js/print-pdf.js
@@ -2,5 +2,5 @@ $(window).load(function () {
 	$.each($('.show-your-work'), function () {
 		if($.trim($(this).val()) === '')$(this).addClass('none');
 	})
-	$('form').append($('<input>', {name : 'html', value :$('.main-content').html()})).submit();
+	$('form').append($('<input>', {type : 'hidden', name : 'html', value :$('.main-content').html()})).submit();
 })	
