c5ab195a45124d6b10597a20df64bc909108e582
Nathan <natethepcspecialist@gmail.com>
2014-09-21 15:58:02 -0400

    update

--- routes/index.js
+++ routes/index.js
@@ -39,7 +39,7 @@ keystone.pre('routes',  function (req, res, next) {
 	console.log('*')
 	console.log('*')
 	console.log('*')
-	console.log(req.host);
+	// console.log(req.host);
 	console.log('*')
 	console.log('*')
 	console.log('*')
