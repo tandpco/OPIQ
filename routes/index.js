ed7883588914095559d1496324346902b8b78179
DudeLetsLive <dannycansignup@gmail.com>
2015-02-24 10:21:24 -0700

    QA Form

--- routes/index.js
+++ routes/index.js
@@ -19,11 +19,11 @@
  */
 
 var _ = require('underscore'),
-	keystone = require('keystone'),
-	middleware = require('./middleware'),
-	importRoutes = keystone.importer(__dirname),
-	url = require('url'),
-	User = keystone.list('User');
+  keystone = require('keystone'),
+  middleware = require('./middleware'),
+  importRoutes = keystone.importer(__dirname),
+  url = require('url'),
+  User = keystone.list('User');
 
 
 
@@ -38,17 +38,17 @@ keystone.set('stripeApiKeyClient', 'pk_live_xzV0TfLXuFF0sHWeon1lkayd'); // Live 
 // keystone.pre('routes',  function (req, res, next) {
 
 
-// 	if(/opportunity/.test(req.headers.host)){
-// 		keystone.set('stripeApiKey', 'sk_live_cSlbqodvJ9gkpQ9030kwv46v'); // Live ENV key
-// 		keystone.set('stripeApiKeyClient', 'pk_live_xzV0TfLXuFF0sHWeon1lkayd'); // Live client ENV key
-		
-// 	}else{
-// 		keystone.set('stripeApiKey', 'sk_test_His9L7RGJvdVRuuPOkCeuand'); // Test ENV key
-// 		keystone.set('stripeApiKeyClient', 'pk_test_SxLXrzbxiAiTwnt8qiOW1agS'); // Test client ENV key	
-// 	}
+//  if(/opportunity/.test(req.headers.host)){
+//    keystone.set('stripeApiKey', 'sk_live_cSlbqodvJ9gkpQ9030kwv46v'); // Live ENV key
+//    keystone.set('stripeApiKeyClient', 'pk_live_xzV0TfLXuFF0sHWeon1lkayd'); // Live client ENV key
+    
+//  }else{
+//    keystone.set('stripeApiKey', 'sk_test_His9L7RGJvdVRuuPOkCeuand'); // Test ENV key
+//    keystone.set('stripeApiKeyClient', 'pk_test_SxLXrzbxiAiTwnt8qiOW1agS'); // Test client ENV key  
+//  }
 
-	
-// 	next();
+  
+//  next();
 // });
 
 keystone.pre('routes', middleware.initLocals);
@@ -61,174 +61,174 @@ keystone.pre('render', middleware.flashMessages);
 
 // Import Route Controllers
 var routes = {
-	views: importRoutes('./views'),
-	updatedb: importRoutes('./updatedb'),
-	lib : importRoutes('./lib'),
-	api: importRoutes('./api')
+  views: importRoutes('./views'),
+  updatedb: importRoutes('./updatedb'),
+  lib : importRoutes('./lib'),
+  api: importRoutes('./api')
 };
 
 
 function get_message (req, res) {
-	console.log(req.body);
+  console.log(req.body);
 }
 
 // Setup Route Bindings
 exports = module.exports = function(app) {
-	// Session
-	app.all('/:route', middleware.forceSSL);
-	app.all('/', middleware.forceSSL);
+  // Session
+  app.all('/:route', middleware.forceSSL);
+  app.all('/', middleware.forceSSL);
 
-	// Views
-	app.get('/', routes.views.analysis);
-	app.get('/allanswers', routes.updatedb.getallanswers);
-	app.get('/report', routes.views.report);
-	app.get('/signout', routes.views.signout);
-	app.get('/industry', routes.views.content);
-	app.get('/financial', routes.views.content);
-	app.get('/operational', routes.views.content);
-	app.get('/product-services', routes.views.content);
-	app.get('/customer', routes.views.content);
-	app.get('/tos', routes.views.tos);
-	app.get('/register-success', routes.views.register_success);
-	app.post('/coupon', routes.lib.coupon);
-	app.post('/logged', routes.updatedb.checklogged);
-	app.post('/bklog', routes.lib.newbl);
-	app.post('/savequestion', routes.updatedb.savequestion);
-	app.post('/pdf', routes.lib.pdf);
-	app.post('/charge', routes.lib.charge);
-	app.post('/message', get_message);
-	app.get('/forgot-page', routes.views.forgotPage);
-	app.all('/print', routes.views.printable);
-	app.post('/help', function(req, res) {
-		var nodemailer = require("nodemailer");
-		// create reusable transport method (opens pool of SMTP connections)
-		var smtpTransport = nodemailer.createTransport("SMTP",{
-		    service: "Mandrill",
-		    auth: {
-		        user: "opiq@launchthought.com",
-		        pass: "0hhMPW05BM24YY73g7KzOg"
-		    }
-		});
-		// setup e-mail data with unicode symbols
-		var mailOptions = {
-		    from: "OpportunityIQ <opiq@launchthought.com>", // sender address
-		    to: "grantmwebster@gmail.com", // list of receivers
-		    subject: "OPIQ " + req.body.page, // Subject line
-			html: '<strong>' + req.body.page + '</strong><br /><br />' + req.body.question + '<br /><br /><b>From:</b> ' + req.body.user + '<br><b>Email Address:</b> ' + req.body.email // html body
-		}
-		// send mail with defined transport object
-		smtpTransport.sendMail(mailOptions, function(error, response){
-		    if(error){
-		        console.log(error);
-		    }else{
-		        res.send(true);
-		    }
+  // Views
+  app.get('/', routes.views.analysis);
+  app.get('/allanswers', routes.updatedb.getallanswers);
+  app.get('/report', routes.views.report);
+  app.get('/signout', routes.views.signout);
+  app.get('/industry', routes.views.content);
+  app.get('/financial', routes.views.content);
+  app.get('/operational', routes.views.content);
+  app.get('/product-services', routes.views.content);
+  app.get('/customer', routes.views.content);
+  app.get('/tos', routes.views.tos);
+  app.get('/register-success', routes.views.register_success);
+  app.post('/coupon', routes.lib.coupon);
+  app.post('/logged', routes.updatedb.checklogged);
+  app.post('/bklog', routes.lib.newbl);
+  app.post('/savequestion', routes.updatedb.savequestion);
+  app.post('/pdf', routes.lib.pdf);
+  app.post('/charge', routes.lib.charge);
+  app.post('/message', get_message);
+  app.get('/forgot-page', routes.views.forgotPage);
+  app.all('/print', routes.views.printable);
+  app.post('/help', function(req, res) {
+    var nodemailer = require("nodemailer");
+    // create reusable transport method (opens pool of SMTP connections)
+    var smtpTransport = nodemailer.createTransport("SMTP",{
+        service: "Mandrill",
+        auth: {
+            user: "opiq@launchthought.com",
+            pass: "0hhMPW05BM24YY73g7KzOg"
+        }
+    });
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+      from: "OpportunityIQ <opiq@launchthought.com>", // sender address
+      to: "daniel@theoryandpractice.co", // list of receivers
+      subject: "OPIQ " + req.body.page, // Subject line
+      html: '<strong>' + req.body.page + '</strong><br /><br />' + req.body.question + '<br /><br /><b>From:</b> ' + req.body.user + '<br><b>Email Address:</b> ' + req.body.email // html body
+    }
+    // send mail with defined transport object
+    smtpTransport.sendMail(mailOptions, function(error, response){
+        if(error){
+            console.log(error);
+        }else{
+            res.send(true);
+        }
 
-		    // if you don't want to use this transport object anymore, uncomment following line
-		    //smtpTransport.close(); // shut down the connection pool, no more messages
-		});
-	});
-	app.post('/forgot', function (req, res) {
-		var view = new keystone.View(req, res);
-	    var email = req.body.email;
-	    var ip = req.connection.remoteAddress;
-		
-		var crypto = require('crypto')
-		  , shasum = crypto.createHash('sha1')
-		var mail = require("nodemailer").mail;
+        // if you don't want to use this transport object anymore, uncomment following line
+        //smtpTransport.close(); // shut down the connection pool, no more messages
+    });
+  });
+  app.post('/forgot', function (req, res) {
+    var view = new keystone.View(req, res);
+      var email = req.body.email;
+      var ip = req.connection.remoteAddress;
+    
+    var crypto = require('crypto')
+      , shasum = crypto.createHash('sha1')
+    var mail = require("nodemailer").mail;
 
-		User.model.find({email : email}).exec(function (i, e) {
-			if(e.length > 0){
-				shasum.update("foo");
-				var uri = shasum.digest('hex');
+    User.model.find({email : email}).exec(function (i, e) {
+      if(e.length > 0){
+        shasum.update("foo");
+        var uri = shasum.digest('hex');
 
-				keystone.set(ip + 'reset', {email : email , token : uri});
+        keystone.set(ip + 'reset', {email : email , token : uri});
 
-				mail({
-				    from: "notifications@opportunityIQ.com", // sender address
-				    to: email, // list of receivers
-				    subject: "Change Password", // Subject line
-				    html: "Click <a href='http://localhost:3000/reset?" + uri + "'>here to change your password</a>"
-				});
-				req.flash('success', 'Check your inbox or spam box for a password reset message.');
-			    view.render('forgot-page');
-			}else{
-				req.flash('error', 'No user found for email address');
-				view.render('forgot-page');
-			}
-		})
+        mail({
+            from: "notifications@opportunityIQ.com", // sender address
+            to: email, // list of receivers
+            subject: "Change Password", // Subject line
+            html: "Click <a href='http://localhost:3000/reset?" + uri + "'>here to change your password</a>"
+        });
+        req.flash('success', 'Check your inbox or spam box for a password reset message.');
+          view.render('forgot-page');
+      }else{
+        req.flash('error', 'No user found for email address');
+        view.render('forgot-page');
+      }
+    })
 
-	});
-	app.get('/reset', function  (req, res) {
-		var view = new keystone.View(req, res);
+  });
+  app.get('/reset', function  (req, res) {
+    var view = new keystone.View(req, res);
 
-		view.render('reset');
-	})
-	app.post('/reset', function (req, res) {
-		var view = new keystone.View(req, res),
-			User = keystone.list('User'),
-			ip = req.connection.remoteAddress;
+    view.render('reset');
+  })
+  app.post('/reset', function (req, res) {
+    var view = new keystone.View(req, res),
+      User = keystone.list('User'),
+      ip = req.connection.remoteAddress;
 
-	    if (!keystone.get(ip + 'reset')) {
-	    	req.flash('error', 'reset token not set');
-	    	view.render('reset');
-	    	return;
-	    }
+      if (!keystone.get(ip + 'reset')) {
+        req.flash('error', 'reset token not set');
+        view.render('reset');
+        return;
+      }
 
-	    var password = req.body.password;
-	    var confirm = req.body.confirm;
-	    if (password !== confirm) {
-	    	req.flash('error', 'passwords do not match');
-	    	view.render('reset');
-	    	return;
-	    }
+      var password = req.body.password;
+      var confirm = req.body.confirm;
+      if (password !== confirm) {
+        req.flash('error', 'passwords do not match');
+        view.render('reset');
+        return;
+      }
 
-	    User.model.findOne({email : keystone.get(ip + 'reset').email}).exec(function (e, u) {
-	    	u.password = password;
-	    	u.save();
-	    })
+      User.model.findOne({email : keystone.get(ip + 'reset').email}).exec(function (e, u) {
+        u.password = password;
+        u.save();
+      })
  
-	    req.flash('success', 'password reset');
-	    keystone.set(ip + 'reset', null);
-	    res.locals.relocate = 'true';
-	    view.render('reset');
-	});
+      req.flash('success', 'password reset');
+      keystone.set(ip + 'reset', null);
+      res.locals.relocate = 'true';
+      view.render('reset');
+  });
 
-	app.post('/questions', routes.views.index);
-	app.get('/questions', function(req, res, next) {
-		// if(req.session.analysisid) {
-		// 	if (req.originalUrl != '/questions')
-		// 		res.redirect('/');
-		// 	else
-		// 		res.writeHead(200);
-		// 		res.pause();
-		// 		console.log(req.headers)
-		// } else {
-		// 	res.redirect('/');
-		// }
-		res.redirect('/');
-	});
-	app.all('/login', routes.views.login);
-	app.all('/register', routes.views.register);
-	app.all('/contact', routes.views.contact);
+  app.post('/questions', routes.views.index);
+  app.get('/questions', function(req, res, next) {
+    // if(req.session.analysisid) {
+    //  if (req.originalUrl != '/questions')
+    //    res.redirect('/');
+    //  else
+    //    res.writeHead(200);
+    //    res.pause();
+    //    console.log(req.headers)
+    // } else {
+    //  res.redirect('/');
+    // }
+    res.redirect('/');
+  });
+  app.all('/login', routes.views.login);
+  app.all('/register', routes.views.register);
+  app.all('/contact', routes.views.contact);
 
-	app.all('/api/v1/users/list', keystone.initAPI, routes.api.users.list);
-	app.all('/api/v1/user/:id', keystone.initAPI, routes.api.users.get);
-	app.all('/api/v1/user/:id/assessments', keystone.initAPI, routes.api.analysis.list);
-	app.all('/api/v1/assessment/:id/:user', keystone.initAPI, routes.api.analysis.answers);
-	app.all('/api/v1/pages/list', keystone.initAPI, routes.api.analysis.pages);
-	app.all('/api/v1/assessment/:id', keystone.initAPI, routes.api.analysis.assessment);
-	app.all('/api/v1/answer/:id/:page', keystone.initAPI, routes.api.analysis.answer);
-	app.all('/api/v1/page/:id', keystone.initAPI, routes.api.analysis.page);
+  app.all('/api/v1/users/list', keystone.initAPI, routes.api.users.list);
+  app.all('/api/v1/user/:id', keystone.initAPI, routes.api.users.get);
+  app.all('/api/v1/user/:id/assessments', keystone.initAPI, routes.api.analysis.list);
+  app.all('/api/v1/assessment/:id/:user', keystone.initAPI, routes.api.analysis.answers);
+  app.all('/api/v1/pages/list', keystone.initAPI, routes.api.analysis.pages);
+  app.all('/api/v1/assessment/:id', keystone.initAPI, routes.api.analysis.assessment);
+  app.all('/api/v1/answer/:id/:page', keystone.initAPI, routes.api.analysis.answer);
+  app.all('/api/v1/page/:id', keystone.initAPI, routes.api.analysis.page);
 
-	// UI Router States
-	app.get('/dashboard', middleware.requireUser, routes.views.dashboard);
-	app.get('/partials/user', routes.views.states.user)
-	app.get('/partials/search', routes.views.states.search)
-	app.get('/partials/assessment', routes.views.states.assessment)
-	app.get('/partials/page', routes.views.states.page)
-	
-	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
-	// app.get('/protected', middleware.requireUser, routes.views.protected);
-	
+  // UI Router States
+  app.get('/dashboard', middleware.requireUser, routes.views.dashboard);
+  app.get('/partials/user', routes.views.states.user)
+  app.get('/partials/search', routes.views.states.search)
+  app.get('/partials/assessment', routes.views.states.assessment)
+  app.get('/partials/page', routes.views.states.page)
+  
+  // NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
+  // app.get('/protected', middleware.requireUser, routes.views.protected);
+  
 }
