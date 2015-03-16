f5a7580aed24848667ec64ae80141052910584bc
DudeLetsLive <dannycansignup@gmail.com>
2015-03-16 15:58:34 -0400

    Testing new Analysis Load Method

--- routes/views/index.js
+++ routes/views/index.js
@@ -28,258 +28,158 @@ exports = module.exports = function(req, res) {
   // ENDIF
 
   if (req.method == 'POST') {
-    // If the request method is POST,
-    // Create a new Analysis from the
-    // Request Object
-    var newAn = new Analysis.model({
-      title : req.body.analysis,
-      user : req.user._id
+    // C R E A T I O N P R O C E S S
+    // =============================
+    // IF User has 0 Analysis OR Has Free Access
+    // * Create the Analysis
+    // ELSE
+    // - IF User Hasn't Paid or Their Year of Access is Up
+    // - * Render the Checkout View
+    // - ELSE (User has paid)
+    // - * Create the Analysis
+    // - ENDIF
+    // ENDIF
+    // =============================
+    Analysis.model.find({user: req.user._id}).exec(function(e, an) {
+      // IF User has 0 Analysis OR Has Free Access
+      if (an.length <= 0 || req.user.freeAccess) {
+        // * Create the Analysis
+        var newAn = new Analysis.model({
+          title : req.body.analysis,
+          user : req.user._id
+        });
+        newAn.save(function(){
+          Analysis.model.findOne({_id: newAn._id}).exec(function(e, an){
+            locals.analysis = an;
+            req.session.analysis = an.title;
+            req.session.analysisid = an._id
+            start(an);
+            res.redirect('/questions/' + an._id);
+          });
+        });
+        // * End Creation
+      // ELSE
+      } else {
+        // - IF User Hasn't Paid or Their Year of Access is Up
+        if (!req.user.oneYearPaidAccess || (Date.now() - req.user.oneYearPaidAccess >=  31536000730)) {
+          // - * Render the Checkout View
+          stripecust.renderCheckout(req, res);
+        // - ELSE (User has paid)
+        } else {
+          // - * Create the Analysis
+          var newAn = new Analysis.model({
+            title : req.body.analysis,
+            user : req.user._id
+          });
+          newAn.save(function(){
+            Analysis.model.findOne({_id: newAn._id}).exec(function(e, an){
+              locals.analysis = an;
+              req.session.analysis = an.title;
+              req.session.analysisid = an._id
+              start(an);
+              res.redirect('/questions/' + an._id);
+            });
+          });
+          // - * End Creation
+        }
+        // ENDIF
+      }
+      // ENDIF
     });
-    newAn.save(function(){
-      Analysis.model.findOne({_id: newAn._id}).exec(function(e, an){
+  } else {
+
+    Analysis.model.findOne({_id: anID}).exec(function(err, an){
+
+      if (err || !an) {
+        res.redirect('/');
+        req.flash('error', 'We were unable to find the opportunity assessment you were looking for.');
+      } else if (!req.user) {
+        res.redirect('/');
+        req.flash('error', 'You do not have the required permissions to view this assessment. Please try logging in or creating an account.');
+      } else if (req.user.isAdmin || an.user === req.user._id) {
         locals.analysis = an;
         req.session.analysis = an.title;
         req.session.analysisid = an._id
         start(an);
-      });
+      } else if (an.user != req.user._id) {
+        res.redirect('/');
+        req.flash('error', 'You do not have the required permissions to view this assessment.');
+      }
     });
-  } else {
-    Analysis.model.findOne({_id: anID}).exec(function(e, an){
-      locals.analysis = an;
-      req.session.analysis = an.title;
-      req.session.analysisid = an._id
-      start(an);
-    });
-  }
 
-  // if(req.user) {
-  
-  //   if(req.body.newa){
-  //     Analysis.model.find({user : req.user._id}).exec(function (e, an) {
-  //       if(an.length === 0 || req.user.freeAccess){
-  //         var a = new Analysis.model({
-  //           title : req.body.analysis,
-  //           user : req.user._id
-  //         });
-  //         a.save(function(){
-  //           Analysis.model.findOne({user: req.user._id, _id: a._id}).exec(function(e, an){
-  //             locals.analysis = an;
-  //             req.session.analysis = an.title;
-  //             req.session.analysisid = an._id
-  //             getPages();         
-  //           })
-  //         });
-  //       }else {
-  //         if(!req.user.oneYearPaidAccess || (Date.now() - req.user.oneYearPaidAccess >=  31536000730))
-  //           stripecust.renderCheckout(req, res);
-  //         else {
-  //           var a = new Analysis.model({
-  //             title : req.body.analysis,
-  //             user : req.user._id
-  //           });
-  //           a.save(function(){
-  //             Analysis.model.findOne({user: req.user._id, _id: a._id}).exec(function(e, an){
-  //               locals.analysis = an;
-  //               req.session.analysis = an.title;
-  //               req.session.analysisid = an._id
-  //               getPages();
-  //             })
-  //           });
-  //         }
-  //       }
-  //     })
-      
-  //   }else{
-  
-  //     Analysis.model.findOne({_id : req.body._id}).exec(function(e, an){
-  //       if(e)console.log(e);
-  //       locals.analysis = an;
-  //       req.session.analysis = an.title;
-  //       req.session.analysisid = an._id;
-  //       start(an);
-  //     });
-  //   }
+  }
 
 function start(an) {
   Answer.model.find({analysis : an._id}).exec(function (err, answers) {
-    if(err)console.log(err);
+    if(err) console.log(err);
     getPages(answers);
   });
 }
 
 function getPages(answers) {
-  Page.model.find().sort('order').exec(function(err, pages) {
-    var cat_totals = {}, 
-        total_pages_answered = 0, 
+  
+  Page.model.find()
+    .sort('order')
+    .exec(function(err, pages) {
+
+      var cat_totals = {},
+        total_pages_answered = 0,
         main_total = 0;
-    for (var i = 0; i < pages.length; i++) {
-      var answer;
-      if(answers) {
-        answer = _.findWhere(answers, {page : pages[i].name});
+
+      for (var i = 0; i < pages.length; i++) {
+
+        var answer;
+        if (answers)
+          answer = _.findWhere(answers, {
+            page: pages[i].name
+          });
+
         locals.categories.cats = _.uniq(_.pluck(pages, 'category'));
-        pages[i].answer =  answer ? answer.answer : 0;
 
-        if(typeof locals.categories[pages[i].category] === 'undefined') {
+
+        pages[i].answer = answer ? answer.answer : 0;
+
+
+        if (typeof locals.categories[pages[i].category] === 'undefined')
           locals.categories[pages[i].category] = [];
-        }
 
-        if(typeof cat_totals[pages[i].category] === 'undefined') {
+
+        if (typeof cat_totals[pages[i].category] === 'undefined') {
+          // console.log(pages[i].category)
           cat_totals[pages[i].category] = {};
           cat_totals[pages[i].category].total = 0;
           cat_totals[pages[i].category].total_pages = 0;
         }
 
-        if(answer) {
+        if (answer) {
           total_pages_answered += 1;
-          cat_totals[ pages[i].category ].total +=  Number(answer.answer);
-          cat_totals[ pages[i].category ].total_pages += 1;
+          cat_totals[pages[i].category].total += Number(answer.answer);
+          // console.log(pages[i].category, answer.answer)
+          cat_totals[pages[i].category].total_pages += 1;
           main_total += Number(answer.answer);
         }
-
         locals.categories[pages[i].category].push(pages[i]);
 
       }
+
       locals.main_total = (main_total / total_pages_answered) * 20;
+
       for (var i in cat_totals) {
-        locals.cat_totals[ i ] = (cat_totals[i].total / cat_totals[i].total_pages) * 20;
+        // console.log(cat_totals[i])
+        locals.cat_totals[i] = (cat_totals[i].total / cat_totals[i].total_pages) * 20;
       }
-      if(answers)
+
+      if (answers)
         locals.answers = answers;
-      locals.pages   = pages;
-    }
-    // Render the view
-    view.render('index');
-  });
-}
 
+      // Set locals
+      locals.pages = pages;
 
-  //           locals.main_total = (main_total / total_pages_answered) * 20;
+      // Render the view
+      view.render('index');
 
-  //           for(var i in cat_totals){
-  //             // console.log(cat_totals[i])
-  //             locals.cat_totals[ i ] = (cat_totals[i].total / cat_totals[i].total_pages) * 20;
-  //           }
-            
-            
-  //           if(answers)
-  //             locals.answers = answers;
-            
-  //           // console.log(answers);
-  //           // Set locals
-  //           locals.pages = pages;
+    });
 
-            
-  //           // Render the view
-  //           view.render('index');
-  //         });
-        
-  //   }
-
-  //   function getPages (answers) {
-    
-  //     Page.model.find()
-  //       .sort('order')
-  //         .exec(function(err, pages) {
-  //             var cat_totals = {}, total_pages_answered = 0, main_total = 0;
-  //             for(var i = 0; i < pages.length; i++){
-  //             var answer;
-  //               if(answers)
-  //                 answer = _.findWhere(answers, {page : pages[i].name});
-
-  //             locals.categories.cats = _.uniq(_.pluck(pages, 'category'));
-
-
-  //             pages[i].answer =  answer ? answer.answer : 0;
-
-
-  //             if(typeof locals.categories[pages[i].category] === 'undefined')
-  //               locals.categories[pages[i].category] = [];
-
-
-  //             if(typeof cat_totals[pages[i].category] === 'undefined'){
-  //               // console.log(pages[i].category)
-  //               cat_totals[pages[i].category] = {};
-  //               cat_totals[pages[i].category].total = 0;
-  //               cat_totals[pages[i].category].total_pages = 0;
-  //             }
-
-  //             if(answer){
-  //               total_pages_answered += 1;
-  //               cat_totals[ pages[i].category ].total +=  Number(answer.answer);
-  //               // console.log(pages[i].category, answer.answer)
-  //               cat_totals[ pages[i].category ].total_pages += 1;
-  //               main_total += Number(answer.answer);
-  //             }
-  //             locals.categories[pages[i].category].push(pages[i]);
-              
-              
-  //           }
-            
-
-
-  //           locals.main_total = (main_total / total_pages_answered) * 20;
-
-  //           for(var i in cat_totals){
-  //             // console.log(cat_totals[i])
-  //             locals.cat_totals[ i ] = (cat_totals[i].total / cat_totals[i].total_pages) * 20;
-  //           }
-            
-            
-  //           if(answers)
-  //             locals.answers = answers;
-            
-  //           // console.log(answers);
-  //           // Set locals
-  //           locals.pages = pages;
-
-            
-  //           // Render the view
-  //           view.render('index');
-  //         });
-        
-  //   }
-    
-  // }else{
-  
-  //   var a = new Analysis.model({
-  //     title : req.body.analysis,
-  //     user : ip
-  //   })
-
-  //   locals.analysis.title = req.body.analysis;
-  //   a.save();
-  //   lgetPages();
-    
-
-  
-  //   function lgetPages () {
-  //     Page.model.find()
-  //     .sort('order')
-  //       .exec(function(err, pages) {
-  //         for(var i = 0; i < pages.length; i++){
-          
-            
-
-  //         locals.categories.cats = _.uniq(_.pluck(pages, 'category'));
-
-  //         pages[i].answer = 0;
-  //         if(typeof locals.categories[pages[i].category] === 'undefined')
-  //           locals.categories[pages[i].category] = [];
-
-
-  //         locals.categories[pages[i].category].push(pages[i]);
-          
-  //       }
-
-
-  //       // Set locals
-  //       locals.pages = pages;
-
-  //       // Render the view
-  //       view.render('index');
-  //     });
-  //   }
-  // }
-    
+  }
 
 }
