8cf14d90df87e7787ca4ec34bccd528cf38d8a15
Daniel Price <dan@danielprice.co>
2016-09-14 09:05:08 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -38,12 +38,12 @@ var _ = require('underscore'),
 
 
 //  if(/opportunity/.test(req.headers.host)){
-//    keystone.set('stripeApiKey', 'sk_live_cSlbqodvJ9gkpQ9030kwv46v'); // Live ENV key
-//    keystone.set('stripeApiKeyClient', 'pk_live_xzV0TfLXuFF0sHWeon1lkayd'); // Live client ENV key
+//    keystone.set('stripeApiKey', ''); // Live ENV key
+//    keystone.set('stripeApiKeyClient', ''); // Live client ENV key
     
 //  }else{
-//    keystone.set('stripeApiKey', 'sk_test_His9L7RGJvdVRuuPOkCeuand'); // Test ENV key
-//    keystone.set('stripeApiKeyClient', 'pk_test_SxLXrzbxiAiTwnt8qiOW1agS'); // Test client ENV key  
+//    keystone.set('stripeApiKey', ''); // Test ENV key
+//    keystone.set('stripeApiKeyClient', ''); // Test client ENV key  
 //  }
 
   
